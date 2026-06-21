b3930bd398c85cf7fdc0f8aa9fd5c44055e2d8b0
Borys <brs.markiv@gmail.com>
2026-06-21 13:32:38 -0700

    Commented espota

--- WiFiConfig.h
+++ WiFiConfig.h
@@ -8,7 +8,7 @@ static constexpr const char* kApSsid = "SetupAP";
 static constexpr const char* kApPassword = "setup123";
 
 // AP mode network settings. Edit kApIp when you want a different portal IP.
-static constexpr const char* kApIp = "10.0.1.1";
+static constexpr const char* kApIp = "10.0.2.1";
 static constexpr const char* kApSubnet = "255.255.255.0";
 
 }  // namespace WiFiConfig
